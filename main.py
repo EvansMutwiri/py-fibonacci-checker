35b4fb6c9369a3a834ad02dbde295ac666073762
EvansMutwiri <mutwirievansm@gmail.com>
2022-02-13 10:48:57 +0300

    Remove commented out code add descriptive print messages

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from math import sqrt
 
 def isPerfectSquare(num):
     s = int(sqrt(num))
-    # if (s * s == num): print('true')
-    # else: print('false')
     return s * s == num
 
 
 def fiboCheck(num):
     if (isPerfectSquare(5 * num * num + 4 or 5 * num * num - 4)):
-        print(str(num) + " is in fibo")
+        print(str(num) + " is in Fibonacci series")
     else:
-        print("not in fibo")
+        print(str(num) + " is not in Fibonacci series")
 
 
 fiboCheck(int(input("Enter number: ")))
