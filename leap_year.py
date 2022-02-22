aade4135aadb5e6b7ed3fa10d3cbc8f3c4f91e0f
EvansMutwiri <mutwirievansm@gmail.com>
2022-02-22 07:25:13 +0300

    Modify condition

--- leap_year.py
+++ leap_year.py
@@ -1,14 +1,17 @@
 def leapYearCheck(year):
     if(year % 100 == 0):
-        return year % 400 == 0
+        if(year % 400 == 0):
+            print(str(year) + " is divisible by both 100 and 400")
+        else:
+            print(str(year) + " is only divisible by 100 but not 400")
     else:
         return year % 4 == 0
 
 year = int(input("Enter year: "))
 
-# print(leapYearCheck(year))
+is_leap = leapYearCheck(year)
 
-if (leapYearCheck == True):
+if (is_leap):
     print(str(year) + " is a leap year")
 else:
     print(str(year) + " is not a leap year")
